5cea5c4e69be3a63a53fa357b762bfc57d97e0cc
sheshkumar <elminous_se44@eluminoustechnologies.com>
2021-02-02 22:46:41 +0530

    added upper limint upto 10 mb

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const indexRoute = require('./routes');
 const app = express();
 
 // middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 app.use(cors());
 
 // route middleware
